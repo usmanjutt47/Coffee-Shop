43fbefaeef1d97f7d32c49cbb64c196a386cbe53
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-11-04 12:49:46 +0500

    Create colors.jsx

---
+++ constants/colors.jsx
@@ -0,0 +1,11 @@
+const Colors = {
+  primaryRed: "#DC3535",
+  secondaryOrange: "#D17842",
+  darkBackground: "#0C0F14",
+  mediumBackground: "#252A32",
+  lightGray: "#52555A",
+  gray: "#AEAEAE",
+  white: "#FFFFFF",
+};
+
+export default Colors;
