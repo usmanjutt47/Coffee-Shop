1213054eec5112f42ffb008139ceb36883ab8dfe
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-11-04 12:49:56 +0500

    Create HeartScreen.jsx

---
+++ screens/HeartScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function HeartScreen() {
+  return (
+    <View>
+      <Text>HeartScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
