83acbac5f4e5ef5627e6cfab9c30802f8de36c20
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-11-12 12:36:07 +0500

    Update userRoute.js

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,11 +4,18 @@ const {
   loginUser,
   getUser,
 } = require("../controllers/userController");
+const {
+  createAddToCart,
+  getAddToCart,
+} = require("../controllers/productsController");
+const upload = require("../middlewares/upload");
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/users", getUser);
+router.post("/add-to-cart", upload.single("image"), createAddToCart);
+router.get("/products/:userId", getAddToCart);
 
 module.exports = router;
