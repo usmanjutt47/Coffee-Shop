a12f532e630544d9f6b7a9cf0b06383ec3613162
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-11-12 12:35:54 +0500

    Create upload.js

---
+++ api/middlewares/upload.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "../uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+module.exports = upload;
