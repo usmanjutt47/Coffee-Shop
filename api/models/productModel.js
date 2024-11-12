b8ff6fcdd6efa794e9ff1e6475f5f759a5a97a86
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-11-12 12:35:59 +0500

    Update productModel.js

--- api/models/productModel.js
+++ api/models/productModel.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String },
+  isCart: { type: Boolean, default: false },
 });
+
+module.exports = mongoose.model("Product", productSchema);
